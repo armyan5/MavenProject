9cf6a23ff46cd0fb786897a25de4d12503349281
Arman <arman-hakobyan@gmx.de>
2024-03-06 19:52:00 +0100

    new

---
+++ src/test/java/com/maven/tests/TestBase.java
@@ -0,0 +1,29 @@
+package com.maven.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+
+public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
